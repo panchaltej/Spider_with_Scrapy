5c2ecf93a8833f2e9ebb167df8fc368299b490c9
panchaltej <tejaspanchal49@gmail.com>
2018-04-13 13:00:23 -0700

    Configured scrapy-splash

--- fed_reserve_jobs/fed_reserve_jobs/spiders/jobsearch.py
+++ fed_reserve_jobs/fed_reserve_jobs/spiders/jobsearch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import scrapy_splash
 
 class JobsearchSpider(scrapy.Spider):
     name = 'fed_reserve_jobs'
@@ -8,5 +8,14 @@ class JobsearchSpider(scrapy.Spider):
     start_urls = ['http://www.federalreserve.gov/start-job-search.htm']
 
     def parse(self, response):
-        pass
+        yield(scrapy_splash.SplashRequest("https://frbog.taleo.net/careersection/1/moresearch.ftl?lang=en&portal=101430233", callback = self.parse_iframe))
+    
+    def parse_iframe(self, response):
+        JOBPOST_SELECTOR = '.titlelink'
+        for jobpost in response.css(JOBPOST_SELECTOR):
+            JOBTITLE_SELECTOR = 'span a ::text'
+            yield {
+              'name' : jobpost.css(JOBTITLE_SELECTOR).extract()
+            }
+      
 
