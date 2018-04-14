bbd507ef76e5ece2edfae3f4345ee0dc55761c17
panchaltej <tejaspanchal49@gmail.com>
2018-04-14 01:07:05 -0700

    Enabled spider to search jobs with keyword

--- fed_reserve_jobs/fed_reserve_jobs/spiders/jobsearch.py
+++ fed_reserve_jobs/fed_reserve_jobs/spiders/jobsearch.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import scrapy_splash
+import json
 
 class JobsearchSpider(scrapy.Spider):
     name = 'fed_reserve_jobs'
@@ -11,11 +12,40 @@ class JobsearchSpider(scrapy.Spider):
         yield(scrapy_splash.SplashRequest("https://frbog.taleo.net/careersection/1/moresearch.ftl?lang=en&portal=101430233", callback = self.parse_iframe))
     
     def parse_iframe(self, response):
-        JOBPOST_SELECTOR = '.titlelink'
-        for jobpost in response.css(JOBPOST_SELECTOR):
+      
+        script = """
+                function main(splash)
+                    local url = splash.args.url
+                    assert(splash:go(url))
+                    assert(splash:wait(0.5))
+
+                    assert(splash:runjs('document.getElementById("advancedSearchInterface.keywordInput").value = "business"'))
+                    assert(splash:runjs('document.getElementById("advancedSearchFooterInterface.searchAction").click()'))
+                    assert(splash:wait(0.5))
+
+                    -- return result as a JSON object
+                    return {
+                        html = splash:html()
+                    }
+                end
+                """
+        url = "https://frbog.taleo.net/careersection/1/moresearch.ftl?lang=en&portal=101430233"
+
+        yield scrapy.Request(url, self.parse_result, meta={
+            'splash': {
+                'args': {'lua_source': script},
+                'magic_response':False,
+                'endpoint': 'execute',
+            }
+        })
+            
+
+    def parse_result(self, response):
+        json_data = json.loads(response.body_as_unicode())
+        data_html = scrapy.Selector(text=json_data["html"], type="html")
+        JOBPOSTS = data_html.xpath('//span[@class="titlelink"]')
+        for jobpost in JOBPOSTS:
             JOBTITLE_SELECTOR = 'span a ::text'
             yield {
-              'name' : jobpost.css(JOBTITLE_SELECTOR).extract()
+                'name' : jobpost.css(JOBTITLE_SELECTOR).extract()
             }
-      
-
